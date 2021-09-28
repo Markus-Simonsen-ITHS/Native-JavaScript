9e5c67c51fc23bd1d6b76c74719b813bb1ca0888
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-09-28 09:55:17 +0200

    Nummer som ord

--- if och else, samt switch/index.js
+++ if och else, samt switch/index.js
@@ -1,5 +1,60 @@
-if(prompt() == "alice" && prompt() == "secret") {
-    alert("Inloggad!")
-} else {
-    alert("Fel användarnamn eller lösenord!")
+let out = ""
+
+let input = prompt()
+
+switch (input) {
+    case '1':
+        // Heal active player
+        out = "ett"
+        break
+    case '2':
+        // Heal active player
+        out = "två"
+        break
+    case '3':
+        // Heal active player
+        out = "tre"
+        break
+    case '4':
+        // Heal active player
+        out = "fyra"
+        break
+    case '5':
+        // Heal active player
+        out = "fem"
+        break
+    case '6':
+        // Heal active player
+        out = "sex"
+        break
+    case '7':
+        // Heal active player
+        out = "sju"
+        break
+    case '8':
+        // Heal active player
+        out = "åtta"
+        break
+    case '9':
+        // Heal active player
+        out = "nio"
+        break
+    case '10':
+        // Heal active player
+        out = "tio"
+        break
+    case '11':
+        // Heal active player
+        out = "elva"
+        break
+    case '12':
+        // Heal active player
+        out = "tolv"
+        break
+    default:
+        // Other roles deals extra damage, but less than mages
+        out = input
+        break
 }
+
+alert(out)
