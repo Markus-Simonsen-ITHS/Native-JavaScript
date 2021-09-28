686033f4599195213fb37b65c8c45a136b1437c8
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-09-28 10:02:17 +0200

    Avdrag för moms: Del 3 (VG)

--- if och else, samt switch/index.js
+++ if och else, samt switch/index.js
@@ -1,9 +1,11 @@
 let price = prompt()
 
-if (price) {
+if (price && !isNaN(price)) {
     let VAT = 0.20
 
     let priceExlVAT = price * (1 - VAT)
 
     alert(priceExlVAT)
+} else if (isNaN(price)) {
+    alert("Du skrev inte in ett nummer.")
 }
