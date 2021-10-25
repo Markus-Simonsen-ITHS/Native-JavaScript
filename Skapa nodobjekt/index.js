a11b94926e9b8808acbdd020ce6ed7e34da9cfc5
Markus Simonsen <33492046+Markussim@users.noreply.github.com>
2021-10-25 19:18:39 +0200

    Skapa nodobjekt

---
+++ Skapa nodobjekt/index.js
@@ -0,0 +1,4 @@
+let puckoChild = document.createElement("li")
+puckoChild.innerText = "puckoBarn"
+
+document.getElementsByTagName("ol")[0].insertBefore(puckoChild, document.getElementsByTagName("li")[2])
